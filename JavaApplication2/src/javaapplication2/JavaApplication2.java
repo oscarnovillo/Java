94f7a0820eb33a5450d9e40eb7053adc73676a92
user <user@Dpt-Info-Carmen>
2017-10-19 08:48:57 +0200

    Merge origin/master
    Conflicts:
    	primerEjemplo/src/main/webapp/index.html

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -5,6 +5,9 @@
  */
 package javaapplication2;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  *
  * @author user
@@ -15,7 +18,28 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code applicahtion logic here
+         Scanner sc = new Scanner(System.in);
+         
+         
+         HashMap m = new HashMap();
+        // primer sumando
+        int sumando1;
+        System.out.print("introduce el primer numero:");
+        sumando1= sc.nextInt();
+        
+        //segundo sumando
+        int sumando2 = 8;
+        System.out.print("introduce el segundo numero:");
+        sumando2= sc.nextInt();
+        
+        //la suma
+        int suma=sumando1+sumando2;
+        //imprimir el resultado
+      
+        
+        System.out.println(sumando1+"+"+sumando2+"="+
+          (sumando1+sumando2));
     }
     
 }
