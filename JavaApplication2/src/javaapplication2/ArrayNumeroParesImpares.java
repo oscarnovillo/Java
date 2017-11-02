88346e3538058eda62176ed82d0eaf89e44fabec
user <user@Dpt-Info-Carmen>
2017-11-02 12:01:54 +0100

    Merge origin/master
    Conflicts:
    	primerEjemplo/src/main/webapp/index.html

---
+++ JavaApplication2/src/javaapplication2/ArrayNumeroParesImpares.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author user
+ */
+public class ArrayNumeroParesImpares {
+    
+    public static void main(String[] args) {
+        //declarar scanner y array numeros
+        
+        //pedir numero de numeros a leer
+        
+        //pedir numeros y meterlos en el array
+        
+        
+        //recorrer array buscando los pares e impares
+        
+        //declarar array pares e impares
+        
+        //recorrer array para meter los pares e impares en su array
+        
+        
+        //recorrer array de pares dar media
+        
+        
+        //recorrer array de impares dar media
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+}
