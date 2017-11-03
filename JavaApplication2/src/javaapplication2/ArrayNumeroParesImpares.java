5b96fd5e3298dd57f6f9ff89adbbc8c3c9523b53
user <user@Dpt-Info-Carmen>
2017-11-03 12:47:54 +0100

    Merge origin/master
    Conflicts:
    	primerEjemplo/src/main/webapp/index.html

--- JavaApplication2/src/javaapplication2/ArrayNumeroParesImpares.java
+++ JavaApplication2/src/javaapplication2/ArrayNumeroParesImpares.java
@@ -5,40 +5,67 @@
  */
 package javaapplication2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author user
  */
 public class ArrayNumeroParesImpares {
-    
+
     public static void main(String[] args) {
         //declarar scanner 
-      
-        
+        Scanner sc = new Scanner(System.in);
+
         //pedir numero de numeros a leer
-        
+        System.out.println("dame numeros a leer");
+        int numeroDeNumeros = sc.nextInt();
         //declarar array de numeros a leer
-  
+        int[] numeros = new int[numeroDeNumeros];
         //pedir numeros y meterlos en el array
-        
-        
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("dame otro numero");
+            numeros[i] = sc.nextInt();
+        }
+
         //recorrer array buscando los pares e impares
-        
+        int numPares = 0;
+        int numImpares = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] % 2 == 0) {
+                numPares++;
+            } else {
+                numImpares++;
+            }
+        }
         //declarar array pares e impares
-        
+        int[] pares = new int[numPares];
+        int[] impares = new int[numImpares];
+
         //recorrer array para meter los pares e impares en su array
-        
+        int indicePares = 0;
+        int indiceImpares = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] % 2 == 0) {
+                pares[indicePares] = numeros[i];
+                indicePares++;
+            } else {
+                impares[indiceImpares] = numeros[i];
+                indiceImpares++;
+            }
+
+        }
         //recorrer array de pares dar media
-        
-        
-        //recorrer array de impares dar media
-        
-        
-        
-        
-        
+        double mediaPares = 0;
+        for (int i = 0; i < pares.length; i++) {
+            mediaPares += pares[i];
+        }
+        //recorrer array de impares dar media   
+        double mediaImpares = 0;
+        for (int i = 0; i < impares.length; i++) {
+            mediaImpares += impares[i];
+        }
+        System.out.println("media de pares es " + mediaPares / numPares);
+        System.out.println("media de impares es " + mediaImpares / numImpares);
     }
-    
-    
-    
 }
