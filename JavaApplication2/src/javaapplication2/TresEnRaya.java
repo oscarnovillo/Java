12ab8bfbca848ac8ba382f1ffaacd26de8f457fb
user <user@Dpt-Info-Carmen>
2017-12-20 13:28:55 +0100

    Merge origin/master
    Conflicts:
    	baseDatos/src/main/java/config/Configuration.java

--- JavaApplication2/src/javaapplication2/TresEnRaya.java
+++ JavaApplication2/src/javaapplication2/TresEnRaya.java
@@ -18,37 +18,54 @@ public class TresEnRaya {
         imprimirTablero(tresenraya);
         for (int i = 0; i < tresenraya.length; i++) {
             for (int j = 0; j < tresenraya[i].length; j++) {
-                tresenraya[j][i]= '-';
+                tresenraya[j][i] = '-';
             }
-           
+
         }
         // poner una coordenada
-        Scanner sc =new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         int y = sc.nextInt();
         tresenraya[x][y] = 'X';
-        
-        
+
         // for de 9 veces, se sale si hay 3 en raya
-        
         // se pide celda a poner, comprobar que no estaba ocupada
-        
         //si esta ocupada pedir otra.
-        
-        // comprobar 3 en raya
+        // comprobar 3 en raya en funcion
+        // dentro de la funcion llamar a funcion comprobarfila, comprobarcolumna
+        // y comprobar diagonales.
         // misma fila
         char turno = 'X';
         boolean ganar = true;
-        
-        for (int i=0; i<3;i++)
-        {
-            if (tresenraya[x][i]!=turno)
-                ganar = false;
-        }
-        
-        
+
         imprimirTablero(tresenraya);
 
+    }
+
+    public static boolean comprobarFila(char[][] tablero, char turno, int x) {
+        boolean tresEnRaya = true;
+        for (int i = 0; i < 3 && tresEnRaya; i++) {
+            if (tablero[x][i] != turno) {
+                tresEnRaya = false;
+            }
+        }
+
+        return tresEnRaya;
+    }
+
+    public static boolean comprobarTresEnRaya(char[][] tablero, char turno,
+      int x, int y) {
+        boolean tresEnRaya = false;
+
+        tresEnRaya = comprobarFila(tablero, turno, x);
+        if (tresEnRaya==false) {
+            tresEnRaya = comprobarFila(tablero, turno, x);
+            if (!tresEnRaya) {
+                tresEnRaya = comprobarFila(tablero, turno, x);
+            }
+        }
+
+        return tresEnRaya;
     }
 
     public static void imprimirTablero(char[][] tablero) {
