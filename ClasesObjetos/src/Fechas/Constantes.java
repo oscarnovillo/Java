6f7aded3cfb9f294496594ae126c0e8e9c90e104
user <user@Dpt-Info-Carmen>
2018-02-19 10:10:16 +0100

    Merge origin/master
    Conflicts:
    	baseDatos/src/main/java/config/Configuration.java

---
+++ ClasesObjetos/src/Fechas/Constantes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fechas;
+
+/**
+ *
+ * @author user
+ */
+public class Constantes {
+    
+    public static final int ADMIN_ID = 1;
+    public static final int NUM_SEGUNDOS_CADUCA = 60;
+    public static final int NUM_SEGUNDOS_BAJA_PRECIO = 10;
+    public static final double FACTOR_BAJA_PRECIO = 0.10;
+    
+}
