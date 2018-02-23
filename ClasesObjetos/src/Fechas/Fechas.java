a7adef0279281c8bed11bd81d5d8de7d92c6fae7
user <user@Dpt-Info-Carmen>
2018-02-23 12:21:54 +0100

    Merge origin/master
    Conflicts:
    	baseDatos/src/main/java/config/Configuration.java

--- ClasesObjetos/src/Fechas/Fechas.java
+++ ClasesObjetos/src/Fechas/Fechas.java
@@ -24,26 +24,39 @@ public class Fechas {
         LocalDate fecha = LocalDate.now();
 
         LocalDate fecha1 = LocalDate.of(2018, 1, 10);
-          
+
         fecha1.plus(10, ChronoUnit.DAYS);
 
         System.out.println(fecha1.format(DateTimeFormatter.ofPattern("dd/MMM/yyyy")));
         System.out.println(fecha.format(DateTimeFormatter.ofPattern("dd/MMM/yyyy")));
-        
+
         Period p = Period.between(fecha, fecha1);
         System.out.println(p.getMonths());
-        
-        
-        LocalDateTime time = LocalDateTime.now();
 
+        LocalDateTime reponedor = LocalDateTime.now();
+
+        // esto simula tiempo
         Scanner sc = new Scanner(System.in);
-        sc.next();
-        LocalDateTime time1 = LocalDateTime.now();
-        
-        System.out.println(time.format(DateTimeFormatter.ofPattern("HH:mm"))+"   "+time1);
-        
-        Duration d = Duration.between(time, time1);
-        System.out.println(d.getSeconds());
+        sc.nextLine();
+        LocalDateTime cajero = LocalDateTime.now();
+
+        System.out.println(reponedor.format(DateTimeFormatter.ofPattern("HH:mm")) + "   " + cajero);
+
+        Duration d = Duration.between(reponedor, cajero);
+        //estos son los seg desde que se repuso.
+        long numSegundos = d.getSeconds();
+        System.out.println("Seg pasados son " + numSegundos);
+        double precioBase = 9.89;
+
+        double descuento = numSegundos / Constantes.NUM_SEGUNDOS_BAJA_PRECIO
+          * Constantes.FACTOR_BAJA_PRECIO * precioBase;
+
+        if (numSegundos > Constantes.NUM_SEGUNDOS_CADUCA) {
+            System.out.println("producto caducado");
+        } else {
+            System.out.println("tienes que pagar" + (precioBase - descuento));
+        }
+
     }
 
 }
