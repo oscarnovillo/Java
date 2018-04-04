9ae6c6aa93b15b04daeb15eb30a48c7a7b7bbe14
user <user@Dpt-Info-Carmen>
2018-04-04 14:05:04 +0200

    Merge origin/master
    Conflicts:
    	baseDatos/src/main/java/config/Configuration.java

--- javafx/src/main/java/fx/FXMLMenuController.java
+++ javafx/src/main/java/fx/FXMLMenuController.java
@@ -74,7 +74,7 @@ public class FXMLMenuController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         try {
             // TODO
-            fxMenu.setVisible(true);
+           
             merchadona = new Merchadona();
             FXMLLoader loader = new FXMLLoader(
               getClass().getResource(Constantes.PANTALLA_SCENE));
@@ -97,6 +97,9 @@ public class FXMLMenuController implements Initializable {
         } catch (IOException ex) {
             Logger.getLogger(FXMLMenuController.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+         fxMenu.setVisible(false);
+        fxRoot.setCenter(scene);
 
     }
 
