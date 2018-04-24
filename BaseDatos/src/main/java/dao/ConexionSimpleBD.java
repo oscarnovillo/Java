4444ad36a268f02f24a0be590c685fdc5eca95b0
oscar <oscar@DESKTOP-E8BOLD9>
2018-04-24 20:39:51 +0200

    Merge origin/master
    Conflicts:
    	HTML5Application/public_html/contentindex.html

---
+++ BaseDatos/src/main/java/dao/ConexionSimpleBD.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import config.Configuration;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Alumno;
+
+/**
+ *
+ * @author oscar
+ */
+public class ConexionSimpleBD {
+
+    // Select JDBC
+    public List<Alumno> getAllAlumnosJDBC() {
+        List<Alumno> lista = new ArrayList<>();
+        Alumno nuevo = null;
+
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            Class.forName(Configuration.getInstance().getDriverDB());
+            Connection connection;
+
+            con = DriverManager.getConnection(
+                    Configuration.getInstance().getUrlDB(),
+                    Configuration.getInstance().getUserDB(),
+                    Configuration.getInstance().getPassDB());
+
+            stmt = con.createStatement();
+            String sql;
+            sql = "SELECT * FROM ALUMNOS";
+            rs = stmt.executeQuery(sql);
+
+            //STEP 5: Extract data from result set
+            while (rs.next()) {
+                //Retrieve by column name
+                int id = rs.getInt("id");
+                String nombre = rs.getString("nombre");
+                Date fn = rs.getDate("fecha_nacimiento");
+                Boolean mayor = rs.getBoolean("mayor_edad");
+                nuevo = new Alumno();
+                nuevo.setFecha_nacimiento(fn);
+                nuevo.setId(id);
+                nuevo.setMayor_edad(mayor);
+                nuevo.setNombre(nombre);
+                lista.add(nuevo);
+            }
+
+        } catch (Exception ex) {
+            Logger.getLogger(AlumnosDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(AlumnosDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(ConexionSimpleBD.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return lista;
+
+    }
+
+}
