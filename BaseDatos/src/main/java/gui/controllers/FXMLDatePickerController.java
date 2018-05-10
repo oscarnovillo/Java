43859d56f0da2464f9f4076bc295b8b3767b38b2
user <user@Dpt-Info-Carmen>
2018-05-10 12:20:57 +0200

    Merge origin/master
    Conflicts:
    	baseDatos/src/main/java/config/Configuration.java

--- BaseDatos/src/main/java/gui/controllers/FXMLDatePickerController.java
+++ BaseDatos/src/main/java/gui/controllers/FXMLDatePickerController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fx.controllers;
+package gui.controllers;
 
 import java.net.URL;
 import java.time.LocalDate;
