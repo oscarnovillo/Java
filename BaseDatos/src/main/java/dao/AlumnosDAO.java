cee70930286911389b69f77e86f4ab45a9a3b477
user <user@Dpt-Info-Carmen>
2018-05-23 13:06:47 +0200

    Merge origin/master
    Conflicts:
    	baseDatos/src/main/java/config/Configuration.java

--- BaseDatos/src/main/java/dao/AlumnosDAO.java
+++ BaseDatos/src/main/java/dao/AlumnosDAO.java
@@ -45,4 +45,108 @@ import org.springframework.transaction.support.DefaultTransactionDefinition;
 public class AlumnosDAO {
 
     
+    //Select DBUtils
+    public List<Alumno> getAllAlumnosDBUtils() {
+        List<Alumno> lista = null;
+       
+        Connection con = null;
+        try {
+            con = DBConnection.getInstance().getConnection();
+
+            QueryRunner qr = new QueryRunner();
+            ResultSetHandler<List<Alumno>> handler
+              = new BeanListHandler<Alumno>(Alumno.class);
+            lista = qr.query(con, "select * FROM ALUMNOS", handler);
+
+        } catch (Exception ex) {
+            Logger.getLogger(AlumnosDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            
+            DBConnection.getInstance().cerrarConexion(con);
+        }
+        return lista;
+    }
+
+    // Con datasource
+    public Alumno getUserById(int id) {
+        Alumno alumno = null;
+
+
+        Connection con = null;
+        try {
+            con = DBConnection.getInstance().getDataSourceFromServer().getConnection();
+            QueryRunner qr = new QueryRunner();
+            ResultSetHandler<Alumno> h
+              = new BeanHandler<>(Alumno.class);
+            alumno = qr.query(con, "select * FROM ALUMNOS where ID = ?",
+              h, id);
+        } catch (Exception ex) {
+            Logger.getLogger(AlumnosDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            DBConnection.getInstance().cerrarConexion(con);
+        }
+        return alumno;
+    }
+
+    
+  
+  
+     // insert DBUTILS
+    public Alumno addUserDBUtils(Alumno alumno, String activacion) {
+   
+        Connection con = null;
+
+        try {
+            con = DBConnection.getInstance().getConnection();
+            con.setAutoCommit(false);
+            QueryRunner qr = new QueryRunner();
+
+            BigInteger id = qr.insert(con,
+              "INSERT INTO LOGIN (USER,PASSWORD,MAIL,ACTIVACION,"
+              + "ACTIVO,FECHA_RENOVACION) VALUES(?,?,?,?,?,now())",
+              new ScalarHandler<BigInteger>(),
+              "", "", "", activacion, 0);
+
+            alumno.setId(id.longValue());
+            con.commit();
+
+        } catch (Exception ex) {
+            try {
+                Logger.getLogger(AlumnosDAO.class.getName()).log(Level.SEVERE, null, ex);
+                if (con!= null)
+                    con.rollback();
+            } catch (SQLException ex1) {
+                Logger.getLogger(AlumnosDAO.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+        } finally {
+            DBConnection.getInstance().cerrarConexion(con);
+        }
+        return alumno;
+
+    }
+
+    public Alumno updateUserDBUtils(Alumno alumno, String activacion) {
+
+        Connection con = null;
+
+        try {
+            con = DBConnection.getInstance().getConnection();
+
+            QueryRunner qr = new QueryRunner();
+
+           int filas = qr.update(con,
+              "UPDATE ALUMNOS SET NOMBRE = ? WHERE ID = ?",
+              alumno.getNombre(),alumno.getId());
+
+
+
+        } catch (Exception ex) {
+            Logger.getLogger(AlumnosDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            DBConnection.getInstance().cerrarConexion(con);
+        }
+        return alumno;
+
+    }
+
 }
